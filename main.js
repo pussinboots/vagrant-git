8ddf4c4a2cd69c201fb89c426ccc90648558a2fb
Frank Ittermann <pussinboots666@googlemail.com>
2014-07-10 20:40:46 +0200

    Code clean up and implements new command line argumnets for vagrant up and provision.
    To call vagrant up execute node main.js --repo owner/repo --up
    To call vagrant provision execute node main.js --repo owner/repo --prov

--- main.js
+++ main.js
@@ -6,15 +6,13 @@ var sys = require('sys');
 var exec = require('child_process').exec;
 var repoFolder = "./";
 
-
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-
 function fetchRepo(owner, repo, callback) {
-	console.log("checkl if exists "+ repoFolder +repo);
+	console.log("check if project exists "+ repoFolder +repo);
 	if (fs.existsSync(repoFolder +repo)) {
 		console.log('git pull ' + repoFolder+'/'+repo);
 		var repository = git(repoFolder+repo);
     	repository.pull('master', function(err, _repo) {
+			sys.puts(err);
     		if(callback)
     			callback(repo)
 	  	})
@@ -22,7 +20,8 @@ function fetchRepo(owner, repo, callback) {
 		console.log('git clone clone https://github.com/' + owner + '/' + repo);
 		git.clone("https://github.com/" + owner+ "/" + repo, repo, function(err, _repo) {
 			sys.puts(err);
-			sys.puts(_repo);
+			if(callback)
+    			callback(repo)
 	  	})
   	}	
 }
@@ -33,6 +32,21 @@ function getVagrantRepo(repo, format) {
 	return {owner:vagrantRepo.split('/')[0], repo:vagrantRepo.split('/')[1]};
 }
 
+function perform(owner, repo, vagrantCmd) {
+	fetchRepo(owner, repo, function(repo) {
+		var vagrantRepo = getVagrantRepo(repo);
+		fetchRepo(vagrantRepo.owner, vagrantRepo.repo, function(repo){
+			var vagrant = exec("vagrant " + vagrantCmd,{cwd: repoFolder +repo}, function (error, stdout, stderr) { 
+				sys.puts(stdout);
+				sys.puts(stderr);
+			});
+			//FIXME added file log
+			vagrant.stdout.on('data', function(data) { process.stdout.write(data); });
+			vagrant.stderr.on('data', function(data) { process.stderr.write(data); });
+		});
+	});
+}
+
 var argv = require('minimist')(process.argv.slice(2));
 console.log('myArgs: ', argv);
 if (argv.repo) {
@@ -40,23 +54,12 @@ if (argv.repo) {
 	var owner = argv.repo.split('/')[0]
 	var repo = argv.repo.split('/')[1]
 	console.log('owner ' + owner + ' repo ' + repo);
-	fetchRepo(owner, repo, function(repo) {
-		var vagrantRepo = getVagrantRepo(repo);
-		fetchRepo(vagrantRepo.owner, vagrantRepo.repo, function(repo){
-			var vagrant = exec("vagrant up",{cwd: repoFolder +repo}, function (error, stdout, stderr) { 
-				sys.puts(stdout);
-				sys.puts(stderr);
-			});
-			vagrant.stdout.on('data', function(data) { process.stdout.write(data); });
-		});
-	});
-}
-
-else if (argv.up) {
-	console.log('vagrant up');
-}
-else if (argv.provision) {
-	console.log('vagrant provision');
-}
-else if (argv.repo) {
+	else if (argv.up) {
+		console.log('vagrant up');
+		perform(owner, repo, "up");
+	}
+	else if (argv.prov) {
+		console.log('vagrant provision');
+		perform(owner, repo, "provision");
+	}
 }
