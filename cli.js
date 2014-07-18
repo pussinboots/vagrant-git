38c508f4dfb33817cdc5db9ea34a9da7aead9221
Frank Ittermann <pussinboots666@googlemail.com>
2014-07-18 11:41:03 +0000

    Added usage description for new options --reponum and --echo.

--- cli.js
+++ cli.js
@@ -12,9 +12,6 @@ function Cli(argv, callback) {
 		var owner = options.repo.split('/')[0]
 		var repo = options.repo.split('/')[1]
 		console.log('owner ' + owner + ' repo ' + repo);
-		if (options.reponum) {
-			//todo implemnet passing repo number so that the prompt will not occur
-		}
 		if (options.up) {
 			console.log('vagrant up');
 			Process().perform(options, owner, repo, "vagrant up", callback);
@@ -22,14 +19,20 @@ function Cli(argv, callback) {
 			console.log('vagrant provision');
 			Process().perform(options, owner, repo, "vagrant provision", callback);
 		} else if (options.echo) {
-			console.log('vagrant provision');
+			console.log("echo 'start vagrant'");
 			Process().perform(options, owner, repo, "echo 'start vagrant'", callback);
 		}else {
 			console.log('default command vagrant up');
 			Process().perform(options, owner, repo, "vagrant up", callback);
 		}
 	} else {
-		console.log('Usage options \n--g (https or git protocol git is default)\n--o (for differennt output folder than working dir optional)  \n--repo (owner/repo for example pussinboots/vagrant-git mandatory) \n--up (to perform vagrant up default command optional) \n--prov (to perform vagrant provision optional)');
+		console.log('Usage options \n--g (https or git protocol git is default)'
+								 +'\n--o (for differennt output folder than working dir optional)'
+								 +'\n--repo (owner/repo for example pussinboots/vagrant-git mandatory)'
+								 +'\n--reponum (if more than one vagrant repo is defined in the project .vagrant.yml file than you can set the repo number to used with this option otherwise it will ask you which one to use. optional)'
+								 +'\n--up (to perform vagrant up default command optional)'
+								 +'\n--prov (to perform vagrant provision optional)'
+								 +'\n--ehoc (perform echo instead of vagrant used for testing optional)');
 	}
 }
 module.exports = Cli;
