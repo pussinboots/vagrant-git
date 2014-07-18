39071bf5cb4ee143ed0b7d77b9fc93501b0f2ab5
Frank Ittermann <pussinboots666@googlemail.com>
2014-07-18 11:47:35 +0000

    Use https git protocol to clone to avoid access with public key.

--- test/clitest.js
+++ test/clitest.js
@@ -6,7 +6,7 @@ var assert = require('assert');
 describe('vgit', function() {
   	describe('cli', function() {
  	    it('should checkout vagrant-git project and vagrant-devel-full', function(done){
- 	    	Cli(['test', '--g', 'https', '--repo', 'pussinboots/vagrant-git', '--reponum', '0','--echo'], function(code, env) {
+ 	    	Cli(['test', './', '--g', 'https', '--repo', 'pussinboots/vagrant-git', '--reponum', '0','--echo'], function(code, env) {
 	          should(code).eql(0,'should have exit code zero');
 	          should(fs.existsSync("./vagrant-devel-full")).eql(true, "vagrant-devel-full folder should exists");
 	          should(fs.existsSync("./vagrant-devel-full/project/vagrant-git")).eql(true, "vagrant-devel-full/project/vagrant-git folder should exists");
